ee0dc872f6556f4908c3a2b3a5aa910a083c4667
AntonioFeitosaAm <acgfnubank@gmail.com>
2022-07-15 13:46:37 -0400

    exercícios com o JAVAScript - Contador de 0 a 10

---
+++ modulo II - HTML - CSS3 - JS/JavaScript/teste.js
@@ -0,0 +1,14 @@
+console.log('Hello Word');
+let evenNums = [];
+function returnEvenValues(array){
+    for(let i = 0; i < array.length; i++) {
+        if(array[i]%2 === 0){
+            evenNums.push(array[i]);
+        }
+    }
+    console.log(evenNums);
+}
+
+let array = [1,2,3,4,5,6,7,8];
+
+returnEvenValues(array);    
