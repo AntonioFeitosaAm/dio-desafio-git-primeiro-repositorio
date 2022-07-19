5c2cae9be927d40c2c4e6728b79ce595586a347d
AntonioFeitosaAm <acgfnubank@gmail.com>
2022-07-19 15:35:22 -0400

    manipulação do DOM e utilização de módulos mjs

---
+++ modulo II - HTML - CSS3 - JS/JavaScript/modulos/main.mjs
@@ -0,0 +1,4 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+console.log(mostraIdade("Antonio", 26));
+console.log(mostraCidade("Antonio", 'Manaus'));
+console.log(mostraHobby('Antonio', 'Ler livros'));
